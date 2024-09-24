50000df4fb77429f3b2cd4755868ac1bac028b28
mohamedabdelfattah44 <jsushjhd@gmail.com>
2024-09-24 20:15:16 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function add(){
+    let firstNum =parseFloat(document.getElementById ("num1").value);
+    let secondNum =parseFloat(document.getElementById ("num2").value);
+    let result =firstNum + secondNum;
+    document.getElementById("result").innerHTML = result;
+}
+function subtract(){
+    let firstNum =parseFloat(document.getElementById ("num1").value);
+    let secondNum =parseFloat(document.getElementById ("num2").value);
+let result = firstNum - secondNum;
+document.getElementById("result").innerHTML = result;
+}
+function mutiply(){
+    let firstNum =parseFloat(document.getElementById ("num1").value);
+    let secondNum =parseFloat(document.getElementById ("num2").value);
+let result = firstNum * secondNum;
+document.getElementById("result").innerHTML = result;
+}
+function Divided(){
+    let firstNum =parseFloat(document.getElementById ("num1").value);
+    let secondNum =parseFloat(document.getElementById ("num2").value);
+let result = firstNum / secondNum;
+document.getElementById("result").innerHTML = result;
+
+}
